3c253dbe1095493478d6fdbd451c895e0beb3b5f
Ankit Lakhlan <ankitlakhlan03@gmail.com>
2023-02-25 16:14:02 +0530

    fixed errors, ready to deploy

--- src/components/pages/TermsAndConditions.jsx
+++ src/components/pages/TermsAndConditions.jsx
@@ -10,7 +10,8 @@ const TermsAndConditions = (props) => {
                     <h3>Terms and Conditions</h3>
                     <h2>{props.name}</h2>
                     <p style={{ marginTop: "10px" }}>
-                        These terms and conditions outline the rules and regulations for the use of A. R Enterprise's Website, located at {props.domain}. <br /> <br />
+                        These terms and conditions outline the rules and regulations for the use of N.A. TRADERS
+                        's Website, located at {props.domain}. <br /> <br />
 
                         By accessing this website we assume you accept these terms and conditions. Do not continue to use {props.domain} if you do not agree to take all of the terms and conditions stated on this page.
                         <br /><br />
@@ -18,12 +19,14 @@ const TermsAndConditions = (props) => {
                         The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same. <br /><br />
 
                         <b>Cookies</b> <br />
-                        We employ the use of cookies. By accessing {props.domain}, you agreed to use cookies in agreement with the A. R Enterprise's Privacy Policy.
+                        We employ the use of cookies. By accessing {props.domain}, you agreed to use cookies in agreement with the N.A. TRADERS
+                        's Privacy Policy.
                         <br />
                         Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.
                         <br /><br />
                         <b>License</b> <br />
-                        Unless otherwise stated, A. R Enterprise and/or its licensors own the intellectual property rights for all material on {props.domain}. All intellectual property rights are reserved. You may access this from {props.domain} for your own personal use subjected to restrictions set in these terms and conditions.
+                        Unless otherwise stated, N.A. TRADERS
+                        and/or its licensors own the intellectual property rights for all material on {props.domain}. All intellectual property rights are reserved. You may access this from {props.domain} for your own personal use subjected to restrictions set in these terms and conditions.
                         <br /><br />
                         You must not: <br /><br />
 
@@ -33,9 +36,13 @@ const TermsAndConditions = (props) => {
                         Redistribute content from {props.domain} <br />
                         This Agreement shall begin on the date hereof. <br />
 
-                        Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. A. R Enterprise does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of A. R Enterprise,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, A. R Enterprise shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website. <br /><br />
+                        Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. N.A. TRADERS
+                        does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of N.A. TRADERS
+                        ,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, N.A. TRADERS
+                        shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website. <br /><br />
 
-                        A. R Enterprise reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions. <br /><br />
+                        N.A. TRADERS
+                        reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions. <br /><br />
 
                         You warrant and represent that: <br /><br />
 
@@ -43,7 +50,8 @@ const TermsAndConditions = (props) => {
                         The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party; <br />
                         The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy <br />
                         The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity. <br />
-                        You hereby grant A. R Enterprise a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media. <br /> <br />
+                        You hereby grant N.A. TRADERS
+                        a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media. <br /> <br />
 
                         <b>Hyperlinking to our Content </b> <br />
                         The following organizations may link to our Website without prior written approval: <br />
@@ -64,19 +72,22 @@ const TermsAndConditions = (props) => {
                         internet portals; <br />
                         accounting, law and consulting firms; and <br />
                         educational institutions and trade associations. <br />
-                        We will approve link requests from these organizations if we decide that: <br />(a) the link would not make us look unfavorably to ourselves or to our accredited businesses; <br /> (b) the organization does not have any negative records with us;<br /> (c) the benefit to us from the visibility of the hyperlink compensates the absence of A. R Enterprise; and <br /> (d) the link is in the context of general resource information.
+                        We will approve link requests from these organizations if we decide that: <br />(a) the link would not make us look unfavorably to ourselves or to our accredited businesses; <br /> (b) the organization does not have any negative records with us;<br /> (c) the benefit to us from the visibility of the hyperlink compensates the absence of N.A. TRADERS
+                        ; and <br /> (d) the link is in the context of general resource information.
 
                         These organizations may link to our home page so long as the link: <br /> (a) is not in any way deceptive;
                         <br /> (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and <br /> (c) fits within the context of the linking party’s site. <br /><br />
 
-                        If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to A. R Enterprise. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response. <br /> <br />
+                        If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to N.A. TRADERS
+                        . Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response. <br /> <br />
 
                         Approved organizations may hyperlink to our Website as follows: <br /><br />
 
                         By use of our corporate name; or <br /><br />
                         By use of the uniform resource locator being linked to; or <br /><br />
                         By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party’s site. <br /><br />
-                        No use of A. R Enterprise's logo or other artwork will be allowed for linking absent a trademark license agreement. <br /><br />
+                        No use of N.A. TRADERS
+                        's logo or other artwork will be allowed for linking absent a trademark license agreement. <br /><br />
 
                         <b> iFrames</b> <br />
                         Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website. <br /><br />
